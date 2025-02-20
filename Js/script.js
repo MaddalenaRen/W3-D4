92759b6ba279a1209ecc9d6539ca49de35288d8b
MaddalenaRen <ileniarennella@gmail.com>
2025-02-20 23:51:36 +0100

    complete

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,51 @@
-const cells = []
+// inseriamo una tabella
 
-for (let i = 0; i < 76; i++) {
-    cell = document.createElement('div')
-    cell.classList.add('cell')
-    cell.innertext = i
-    cells[i] = cell
+let numeriRimasti = []
+
+for (let i = 1; i <= 90; i++) {
+    numeriRimasti.push(i)
 }
+
+const addTable = () => {
+    const table = document.getElementById('tabella')
+    table.innerHTML = ''
+
+
+    for (let i = 1; i <= 90; i++) {
+        if ((i - 1) % 10 === 0) {
+            row = document.createElement('tr');
+            table.appendChild(row);
+        }
+        const cell = document.createElement('td')
+        cell.innerText = (i)
+        cell.setAttribute('id', "num-" + i)
+        table.appendChild(cell)
+
+
+    }
+
+}
+
+addTable()
+// voglio estrarre un numero random e segnarlo sull cartellone della tombola
+
+function estractNumber() {
+
+
+
+    const numeroTombola = Math.floor((Math.random() * numeriRimasti.length))
+
+
+    const numVero = numeriRimasti[numeroTombola];
+
+
+    console.log(numVero)
+
+
+    const selected = document.getElementById("num-" + numeriRimasti[numeroTombola])
+    selected.classList.add('estratto')
+
+    numeriRimasti.splice(numeroTombola, 1)
+
+}
+
